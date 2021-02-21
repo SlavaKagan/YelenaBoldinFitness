11ed96440cc47061c89ff0787c403fada12655a9
Slava Kagan <slava15.92@gmail.com>
2021-02-21 11:36:57 +0200

    Add menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './assets/YBLogo.png';
+import facebook from './assets//Social/facebook.png';
+import instagram from './assets/Social/instagram.png';
+
 import './App.css';
-import {Example} from './components/Menu/Menu';
+import {MenuBar} from './components/Menu/Menu';
+import {About} from './Pages/AboutMe/About';
+import {Contact} from './Pages/Contact/Contact';
+import {Cake} from './Pages/Cakes/Cakes';
+import {Gallery} from './Pages/Gallery/Gallery';
+import {Naturopathy} from './Pages/Naturopathy/Naturopathy';
+import {Training} from './Pages/Training/Training';
+import {Home} from './Pages/Home/Home';
+import {Clips} from './Pages/Clips/Clips';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <img src={logo} alt="logo" />
-        <Example></Example>
-        <p>
-          ילנה בולדין fdsfdssfdsfsdds
-            fsdfdsfsd
-            fdssdfds
-            fsdsdfsd
-            dfss
-            מאמנת מוסמכת באיזור המרכז
-        </p>
+         <img src={logo} alt="logo3" />
+         <a href="https://www.instagram.com/yelenaboldin/"> <img src={instagram} alt="logo1" /> </a>
+        <a href="https://www.facebook.com/lena.boldin"> <img src={facebook} alt="logo2" /> </a>
+        <MenuBar></MenuBar>
+          <Home></Home>
+        <About></About>
+          <Training></Training>
+          <Naturopathy></Naturopathy>
+          <Gallery></Gallery>
+          <Clips></Clips>
+          <Cake></Cake>
+          <Contact></Contact>
       </header>
     </div>
   );
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,12 +6,13 @@ import {
 } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
 
-export function Example() {
+export function MenuBar() {
 	return (
 		<Menu menuButton={<MenuButton>Open menu</MenuButton>}>
 			<MenuItem>New File</MenuItem>
 			<MenuItem>Save</MenuItem>
 			<MenuItem>Close Window</MenuItem>
 		</Menu>
+
 	);
 }
