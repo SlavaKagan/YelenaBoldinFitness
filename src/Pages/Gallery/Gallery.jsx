8eeac8e1f4a17b5ec59a2319c274e15884664333
Slava Kagan <slava15.92@gmail.com>
2021-02-22 11:11:01 +0200

    fix package json

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -5,21 +5,16 @@ import YB2 from '../../assets/Gallery/YB2.png';
 import YB3 from '../../assets/Gallery/YB3.jpeg';
 import YB4 from '../../assets/Gallery/YB4.png';
 
-const row =`
-	display: flex;
-    flex-wrap: wrap;
-    padding: 0 4px;
-`;
-
 export function Gallery() {
     return (
-        <row>
-            <div className="column">
-                <img src={YB1} style="width:100%"/>
-                <img src={YB2} style="width:100%"/>
-                <img src={YB3} style="width:100%"/>
-                <img src={YB4} style="width:100%"/>
-            </div>
-        </row>
+        <div className="App">
+            <p>
+                עוגות
+            </p>
+            <img src={YB1} alt="logo" width="15%" height="10%"/>
+            <img src={YB2} alt="logo" width="15%" height="10%"/> <br/> <br/>
+            <img src={YB3} alt="logo" width="15%" height="10%"/>
+            <img src={YB4} alt="logo" width="15%" height="10%"/>
+        </div>
     );
 }
